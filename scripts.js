2e8e5aae5603ff8c5bef411f935623a2ee6a28b7
SabaDodashvili <sabadodashvili777@gmail.com>
2021-08-19 18:25:33 +0400

    add gameEnd func and confetti effect

--- scripts.js
+++ scripts.js
@@ -19,11 +19,14 @@ initialState('light-theme');
 // ================================================ H A N G M A N ========================================================
 
 window.onload = function () {
-  let wordsArr = ['programming', 'landing', 'cabbage', 'snake', 'interruption'];
+  let end = Date.now() + 1 * 1000;
+  let colors = ['#12db12', '#e817ff', '#13dde8', '#fced17', '#ffa826'];
+  let wordsArr = ['programming', 'landing', 'awkward', 'snake', 'interruption', 'bayou', 'galvanize', 'nowadays'];
   let randomWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
   let wrongLetters = document.querySelector('.wrong-letters__board');
   let humanElements = document.querySelectorAll('.human-element');
   let tryCounter = 0;
+  let gameEnd = false;
   let letterInput = ``;
   let buttonKey;
 
@@ -37,13 +40,9 @@ window.onload = function () {
   document.addEventListener('keyup', (e) => {
     let regxp = /[a-z|A-Z]/;
     buttonKey = e.key.toLowerCase();
-    let guessedLetters = '';
-    for (let i = 0; i < inputWordLettersArr.length; i++) {
-      guessedLetters += inputWordLettersArr[i].textContent;
-    }
-    if (regxp.test(buttonKey) && buttonKey.length == 1 && randomWord.indexOf(buttonKey) !== -1 && guessedLetters.indexOf(buttonKey) == -1) {
+    if (regxp.test(buttonKey) && buttonKey.length == 1 && randomWord.indexOf(buttonKey) !== -1 && !gameEnd) {
       documentActions(e);
-    } else if (regxp.test(buttonKey) && buttonKey.length == 1 && wrongLetters.textContent.indexOf(buttonKey) == -1 && guessedLetters.indexOf(buttonKey) == -1) {
+    } else if (regxp.test(buttonKey) && buttonKey.length == 1 && wrongLetters.textContent.indexOf(buttonKey) == -1 && !gameEnd) {
       wrongLetters.textContent += buttonKey + ', ';
       humanElements[tryCounter].style.display = 'block';
       tryCounter += 1;
@@ -58,19 +57,26 @@ window.onload = function () {
                 <span></span>
               </span>
               <span class="gibbet-head__mouth"></span>
-              <span class="gibbet-head__tongue"></span>
-							`;
+          <span class="gibbet-head__tongue"></span>`;
+        gameEnd = true;
       }
     }
   });
+
   function documentActions(e) {
     let indexsArr = getListIdx(randomWord, buttonKey);
+
     for (let i = 0; i < inputWordLettersArr.length; i++) {
       for (let k = 0; k < indexsArr.length; k++) {
         if (indexsArr[k] === i) {
           inputWordLettersArr[i].textContent = buttonKey;
         }
       }
+    }
+
+    if (checkGuessLetters() === false) {
+      doConfetti();
+      gameEnd = true;
     }
   }
 
@@ -81,5 +87,39 @@ window.onload = function () {
       listIdx.push(lastIndex);
     }
     return listIdx;
+  }
+
+  function checkGuessLetters() {
+    let notFilled = false;
+
+    for (let i = 0; i < inputWordLettersArr.length; i++) {
+      if (!inputWordLettersArr[i].textContent) {
+        notFilled = true;
+        break;
+      }
+    }
+
+    return notFilled;
+  }
+
+  function doConfetti() {
+    confetti({
+      particleCount: 170,
+      angle: 70,
+      spread: 140,
+      origin: { x: 0 },
+      colors: colors,
+    });
+    confetti({
+      particleCount: 170,
+      angle: 140,
+      spread: 140,
+      origin: { x: 1 },
+      colors: colors,
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
   }
 };
